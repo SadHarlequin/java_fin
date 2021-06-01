339acd63ae70bb963203c70034c88cce09df64b5
Mikhail <56535458+SadHarlequin@users.noreply.github.com>
2021-06-02 02:53:30 +0300

    Add files via upload

---
+++ java-templates-master/java-templates-master/practice23/src/main/java/com/example/practice23/EmailInfo.java
@@ -0,0 +1,7 @@
+package com.example.practice23;
+
+public class EmailInfo {
+    public static final String email = "ytkonoszhora";
+    public static final String email_sender = "ytkonoszhora";
+    public static final String email_password = "Sd2568Grom";
+}
